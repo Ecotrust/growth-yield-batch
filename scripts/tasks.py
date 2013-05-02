792b2202cfdf449e91f2aa24a02c928c1703f319
Matthew Perry <mperry@ecotrust.org>
2013-05-02 06:32:49 -0700

    Better logging, error handling and output

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -1,8 +1,6 @@
 from celery import Celery, current_task
 import sys
 import os
-import time
-import random
 import subprocess
 
 
@@ -11,26 +9,27 @@ sys.path.append("/var/celery")
 sys.path.append("/usr/local/apps/growth-yield-batch/scripts")
 celery.config_from_object('celeryconfig')
 
+
 from models import Task, db
 
 
-@celery.task
-def add(x, y):
-    time.sleep(random.randint(0, 4))
-    return x + y
+class FVSException(Exception):
+    pass
 
 
-@celery.task
-def square(z):
-    time.sleep(random.randint(0, 4))
-    return z ** 2
+OUTDIR = '/usr/local/data/out'
 
 
+def write_err(uid, err):
+    outerr = os.path.join(OUTDIR, uid + ".err")
+    with open(outerr, 'w') as fh:
+        fh.write(err)
 
 
 @celery.task
 def fvs(datadir):
     assert os.path.isdir(datadir)
+    uid = os.path.basename(datadir)
 
     args = ['/usr/local/bin/fvs', datadir]
     print "Running %s" % ' '.join(args)
@@ -39,31 +38,36 @@ def fvs(datadir):
     print fvsout  # how to stream this?
     print fvserr
     if proc.returncode != 0:
-        raise Exception("fvs('%s') celery task failed ######## OUT ### %s ####### ERR ### %s" % (datadir, fvsout, fvserr))
+        err = "fvs('%s') celery task failed ######## OUT ### %s ####### ERR ### %s" % (datadir, fvsout, fvserr)
+        write_err(uid, err)
+        raise FVSException(err)
 
-    uid = os.path.basename(datadir)
     tmpdir = os.path.join('/tmp/', uid)  # .replace("_", "/")
     assert os.path.isdir(tmpdir)
 
     # TODO more error checking
-    # make sure we have 6 out files and 6 trl files? 
+    # make sure we have 6 out files and 6 trl files?
     # if not pass_tests():
     #     raise Exception("Tests failed")
 
     # parse data from fvs outputs
-    outcsv = os.path.join('/usr/local/data/out/', uid + ".csv")
+    outcsv = os.path.join(OUTDIR, uid + ".csv")
     import extract
     df = extract.extract_data(tmpdir)
     df.to_csv(outcsv)
     if not os.path.exists(outcsv):
-        raise Exception("%s not created" % outcsv)
+        err = "FVS ran in %s but %s was not created" % (tmpdir, outcsv)
+        write_err(uid, err)
+        raise FVSException(err)
 
     # tar/bzip the files to their final home
-    outbz = os.path.join('/usr/local/data/out/', uid + ".tar.bz")
+    outbz = os.path.join(OUTDIR, uid + ".tar.bz")
     import compress
     compress.tar_bzip2_directory(tmpdir, outbz)
     if not os.path.exists(outbz):
-        raise Exception("%s was not created" % outbz)
+        err = "FVS ran in %s but %s was not created" % (tmpdir, outbz)
+        write_err(uid, err)
+        raise FVSException(err)
 
     # clean up temp data
     import shutil
@@ -72,7 +76,7 @@ def fvs(datadir):
     # Update task record
     request = current_task.request
     task_record = Task.query.filter_by(id=request.id).first()
-    task_record.result = os.path.join('/usr/local/data/out/%s.csv' % uid)
+    task_record.result = os.path.join(OUTDIR, '%s.csv' % uid)
     db.session.commit()
 
     return proc.returncode
