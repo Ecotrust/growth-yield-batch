4c0a01d0c47351066594ab606a0638ac9a1df0dc
Matthew Perry <perrygeo@gmail.com>
2014-02-01 09:14:46 -0800

    fix location sql

---
+++ scripts/blm/fix_location.sql
@@ -0,0 +1,23 @@
+update stands_orig
+set loccode = 606
+where loccode = 708
+and variant = 'WC';
+
+update stands_orig
+set loccode = 618
+where loccode = 709
+and variant = 'WC';
+
+update stands_orig
+set loccode = 615
+where loccode = 710
+and variant = 'WC';
+
+update stands_orig
+set loccode = 610
+where loccode = 711
+and variant = 'WC';
+
+update stands_orig
+set loccode = 612
+WHERE variant = 'PN';
